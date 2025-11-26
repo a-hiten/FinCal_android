5948b749ca2679fcdedbb4c4b09a31a8e8e06458
a-hiten <n.ruri0301@gmail.com>
2025-11-26 16:47:36 +0900

    MedicineActivity 画面デザインで定義したオブジェクトを変数として宣言

--- app/src/main/java/com/example/medmemo/MedicineActivity.kt
+++ app/src/main/java/com/example/medmemo/MedicineActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MedicineActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +23,12 @@ class MedicineActivity : AppCompatActivity() {
         // 画面デザインで定義したオブジェクトを変数として指定する。
         val titleText = findViewById<TextView>(R.id.textView)   // ページのタイトルの部
         titleText.text = "現在使用中のお薬"
+
+        val recyclerView = findViewById<RecyclerView>(R.id.userMedlineRecycle)      // リストの内容は自分のお薬行情報を参照している
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+
+
 
 
     }
