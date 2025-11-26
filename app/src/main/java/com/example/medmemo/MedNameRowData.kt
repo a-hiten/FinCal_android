43422e149c6fef8f3415d11bcb57bfd114ade42f
a-hiten <n.ruri0301@gmail.com>
2025-11-26 16:24:21 +0900

    MedNameRowDataを作成し、データを追加

---
+++ app/src/main/java/com/example/medmemo/MedNameRowData.kt
@@ -0,0 +1,10 @@
+package com.example.medmemo
+
+data class MedNameRowData(
+    // APIのinputパラメータと名前を統一する
+
+    val medName : String,   //薬の名前
+    val data : Int,         //薬の使用期限
+    val effect : String,    //効能・効果
+    val remaining : String, //薬の残り回数
+)
--- app/src/main/java/com/example/medmemo/MedicineActivity.kt
+++ app/src/main/java/com/example/medmemo/MedicineActivity.kt
@@ -1,6 +1,7 @@
 package com.example.medmemo
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,11 @@ class MedicineActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // 画面デザインで定義したオブジェクトを変数として指定する。
+        val titleText = findViewById<TextView>(R.id.textView)   // ページのタイトルの部
+        titleText.text = "現在使用中のお薬"
+
+
     }
 }
