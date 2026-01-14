9c5a8b6ea9c791dd9397f21e406d665e3dbbfe11
a-hiten <n.ruri0301@gmail.com>
2026-01-14 16:35:49 +0900

    メニューを表示できるようにした

--- app/src/main/java/com/example/medmemo/MedAddActivity.kt
+++ app/src/main/java/com/example/medmemo/MedAddActivity.kt
@@ -1,20 +1,30 @@
 package com.example.medmemo
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.medmemo.OverflowMenu.Companion.handleMenuItemSelected
 
-class MedAddActivity : AppCompatActivity() {
+class MedAddActivity : OverflowMenu() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_med_add)
+
+        //追記
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return handleMenuItemSelected(this, item)
     }
 }
--- app/src/main/java/com/example/medmemo/MedicineActivity.kt
+++ app/src/main/java/com/example/medmemo/MedicineActivity.kt
@@ -31,6 +31,11 @@ class MedicineActivity : OverflowMenu() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_medicine)
+
+        //追記
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar2)
+        setSupportActionBar(toolbar)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
